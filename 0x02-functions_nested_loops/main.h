7d1db3db38c2687403305910799c27bb2c2a9873
yemisrach <yemisrachassfa@gmail.com>
2022-09-13 11:59:56 -0700

    1st

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
-#ifindef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#ifindef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
-void print_alphabet(void); 
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,8 +10,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int a,int b);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r,int n);
+
 #endif
