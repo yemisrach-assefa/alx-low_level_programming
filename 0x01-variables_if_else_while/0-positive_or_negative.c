a2bd44b6c15ac3b16bf56604586d6a79e708392a
yemisrach <yemisrachassfa@gmail.com>
2022-09-13 00:33:45 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	printf(" %d is positive\n , n")
+	printf("is positive\n , n");
 }
 else if (n == 0)
 {
-	printf(" %d is zero\n , n")
+	printf("is zero\n , n");
 }
 else
 {
-	printf("%d is negative\n, n")
+	printf("is negative\n , n");
 }
 return (0);
 }
