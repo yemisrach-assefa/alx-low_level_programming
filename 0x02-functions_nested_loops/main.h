c0146fb9152d7e41eedef6abcb223de854e6c0db
yemisrach <yemisrachassfa@gmail.com>
2022-09-13 11:33:34 -0700

    first

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifindef HOLBERTON_H_INCLUDED 
+#ifindef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
@@ -15,4 +15,4 @@ int add(int a,int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r,int n);
-
+#endif
