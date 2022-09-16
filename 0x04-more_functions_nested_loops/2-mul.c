6e0a3de425e886ccbd3d0f475bb2846ea0d6d3e0
yemisrach <yemisrachassfa@gmail.com>
2022-09-16 05:21:48 -0700

    2nd

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * mul(int, int) - function that multiplies two integers
- * @x: first number
- * @y: second number
+ * mul - function that multiplies two integers
+ * @x: An input integer
+ * @y: An input integer
  * Return: Always 0
  */
 int mul(int x, int y)
