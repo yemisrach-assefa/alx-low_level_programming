54e7941b39a174a4ca661520ed28bafbc31c8fd3
yemisrach <yemisrachassfa@gmail.com>
2022-09-16 05:13:26 -0700

    2nd

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * mul(int, int) - function that multiplies two integers
- * @a: first number
- * @b: second number
- * Return: return nothing
+ * @x: first number
+ * @y: second number
+ * Return: Always 0
  */
 int mul(int x, int y)
 {
-	prod = mul(a,b);
-	printf("Product of %d and %d is: %d", a, b, prod);
-	return(x * y);
+	return (x * y);
 }
