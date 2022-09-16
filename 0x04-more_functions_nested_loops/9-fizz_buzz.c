9eadc10c1b3bf7d5db5c1039acf8041e8634890d
yemisrach <yemisrachassfa@gmail.com>
2022-09-16 13:54:26 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,21 +5,20 @@
  * main - Entry piont
  * Return: Always 0
  */
-
 int main(void)
 {
 	int i = 1;
 
-	for (; i < 100; i++)
+	for (; i < 100 ; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	printf("Buzz\n");
 
