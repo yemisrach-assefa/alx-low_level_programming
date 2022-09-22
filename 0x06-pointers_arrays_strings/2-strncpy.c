9c897348c64d72ab1c28c66a5895ecfa947cdc56
yemisrach <yemisrachassfa@gmail.com>
2022-09-22 06:05:29 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strncpy - a function that copys the strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-char *_strncpy(char *dest, char *src, int n)
+int main(void)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	char s1[98];
+	char *ptr;
+	int i;
 
-	while (*src)
+	for (i = 0; i < 98 - 1; i++)
 	{
-		srclen++;
-		src++;
+	s1[i] = '*';
 	}
-
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", ptr);
+	for (i = 0; i < 98; i++)
+	{
+	if (i % 10)
+	{
+		printf(" ");
+	}
+	if (!(i % 10) && i)
+	{
+	printf("\n");
+	}
+	printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
 }
